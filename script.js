33f933e01d7fb08a4f13ce1ec9f28c5eaac00fa2
Barri <info@bdesignstudio.co.uk>
2020-05-20 20:12:50 +0100

    other validation functions

--- script.js
+++ script.js
@@ -18,9 +18,20 @@ function showSuccess(input){
   formControl.className = 'form-control success';
 }
 //Email checker 
-function isValid(email){
+function checkEmail(input){
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLocaleLowerCase());
+  // return re.test(String(email).toLocaleLowerCase());
+  if(re.test(input.value.trim())){
+    showSuccess(input);
+  } else {
+    showError(input, `Email is not valid`);
+  }
+}
+//check password match
+function checkPasswordsMatch( input1, input2){
+  if (input1.value !== input2.value) {
+    showError(input2, 'Password do not match');
+  }
 }
 //check required function
 function checkRequired(inputArr) {
@@ -33,43 +44,31 @@ function checkRequired(inputArr) {
     }
   });
 }
-// Get field
+
+// check length
+function checkLength(input, min, max) {
+  if(input.value.length < min){
+    showError( input , `${getFieldName(input)} must be at least ${min} character`)
+  } else if(input.value.length > max ){
+    showError( input , `${getFieldName(input)} must be less than ${max} character`);
+
+  } else {
+    showSuccess(input);
+  }
+}
+// Get field name and make in capital
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
+
 //Event listeners
 form.addEventListener('submit', function(e){
   e.preventDefault();
  
   checkRequired([username, email, password, password2]);
-//   if(username.value === ''){
-//     showError(username ,'Username is required');
-
-//   }else {
-// showSuccess(username)
-//   }
-
-//   if(email.value === ''){
-//     showError(email ,'Email is required');
-//    } else if(!isValid(email.value)) {
-//     showError(email ,'Email is not valid');
-//   }else {
-// showSuccess(email)
-//   }
-
-//   if(password.value === ''){
-//     showError(password ,'Password is required');
-
-//   }else {
-// showSuccess(password)
-//   }
-
-//   if(password2.value === ''){
-//     showError(password2 ,'Password  is required again');
-
-//   }else {
-// showSuccess(password2)
-//   }
-
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password , password2 )
 });
 
